6287c9fb910c1d858694fa14b15f90bfe1ad1b8b
Reteam.id <81605818+reza838@users.noreply.github.com>
2022-04-10 20:20:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
+global.owner = ['6288217843188','6288217843188'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['6288217843188'] //another owner number
+global.premium = ['6288217843188'] //premium number
+global.pengguna = 'Reza Temayang' //username
+global.botnma = 'Bot rex' //bot name
 global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.packname = 'Bot rex' //sticker package name
+global.author = 'By Reza' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
